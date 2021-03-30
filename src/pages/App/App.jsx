93a775645c27f21488a39d9eaec8eb5c017eafcb
Jorge Ossa <jorgehossa@gmail.com>
2021-03-30 01:17:20 -0500

    🐛 FIX: Remove CRA default assets

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './App.styles';
+
+const App = (props) => (
+  <div className="AppWrapper">
+    Test content
+  </div>
+);
+
+App.propTypes = {
+  // bla: PropTypes.string,
+};
+
+App.defaultProps = {
+  // bla: 'test',
+};
+
+export default App;
